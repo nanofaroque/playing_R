cd10ff4c504f4a1c92552d1e3c7e8a69454eaf6c
ofaroque <ofaroque@amazon.com>
2020-04-20 16:57:37 -0500

    learning R

---
+++ data_types.R
@@ -0,0 +1,19 @@
+# creating vectors
+apple <- c('apple','amazon') # c is used for combine into vector
+print(apple)
+
+print('printing list')
+appleList <- list('apple','amazon')
+print(appleList)
+
+print('creating matrix')
+M = matrix(c('a','b','c','d','e','f'),2,3,byrow=TRUE)
+print(M)
+
+print('creating data frame')
+BMI <- data.frame(
+  gender=c('male','male','female'),
+  height=c(152,155,148),
+  age=c(23,24,25)
+)
+print(BMI)
